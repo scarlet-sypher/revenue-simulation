88b8d9edda5fc50e89a6eff219a99f97e0e4f9df
Scarlet Sypher <ayushjha21157@gmail.com>
2026-04-03 13:54:58 +0530

    modified: implemented logic to split data by quarter

--- server/src/services/data.ts
+++ server/src/services/data.ts
@@ -1,9 +1,48 @@
 import { loadDeals } from "../utils/csv.ts";
 import type { Deal } from "../types/deal.ts";
+import { getMonth } from "../utils/date.ts";
 
-export const getAllDeals = async () : Promise <Deal[]> => {
+export const getAllDeals = async () : Promise <Deal[]>  => {
 
-  const deals = await loadDeals();
+  return await loadDeals();
 
-  return deals;
-} ;
+};
+
+export const splitDeals = ( deals: Deal[] ) => {
+
+  const q1: Deal[] = []; // jan - march
+  const q2: Deal[] = []; // april - june
+  const q3: Deal[] = []; // july to sept
+
+
+
+  for (const d of deals) {
+
+    if (d.closed_date) {
+
+      const m = getMonth(d.closed_date);
+
+      if (!m) continue;
+
+      if (m >= 1 && m <= 3) q1.push(d); // Q1
+
+
+      else if (m >= 4 && m <= 6) q2.push(d); // Q2
+
+
+    } 
+    
+    else {
+
+      const m = getMonth(d.created_date);
+
+      if (!m) continue;
+
+      if (m >= 7 && m <= 9) q3.push(d); //Q3 (dont have closing date)
+    }
+
+
+  }
+
+  return { q1, q2, q3 };  // returns data for all 3 quarters
+};
